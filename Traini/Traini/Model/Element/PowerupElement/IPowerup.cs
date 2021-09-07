3719d36279e53136c04f22b40671c9eed29bc822
RicaBiz <biliardelli@hotmail.it>
2021-09-07 23:36:37 +0200

    Added comments on Util and Element

--- Traini/Traini/Model/Element/PowerupElement/IPowerup.cs
+++ Traini/Traini/Model/Element/PowerupElement/IPowerup.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace Traini.Model.Element.PowerupElement
 {
+    /// <summary>
+    /// Please note that this interface, and the relative implementations, are not my competence
+    /// in the project goals.
+    /// This is used only for simulating the interactions Level.
+    /// </summary>
     public interface IPowerup : IElement
     {
     }
